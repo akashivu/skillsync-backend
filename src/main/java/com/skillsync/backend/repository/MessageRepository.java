258c5e41ecc0e7b089efe16193f3ed50b3e048e9
Akash Patil <akashivu002@gmail.com>
2025-08-02 11:17:57 +0530

    feat: Add MessageRepository with method to fetch messages by team ordered by timestamp

---
+++ src/main/java/com/skillsync/backend/repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.skillsync.backend.repository;
+
+import com.skillsync.backend.model.Message;
+import com.skillsync.backend.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByTeamOrderByTimestampAsc(Team team)
+
+}
